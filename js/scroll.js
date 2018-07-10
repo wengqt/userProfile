a32c2e893aab0e83c3966e15697b244644d35de8
weng <1906338209@qq.com>
2018-07-10 08:56:57 +0800

    picture

---
+++ js/scroll.js
@@ -0,0 +1,51 @@
+
+window.onload=function () {
+     checkScroll();
+    //console.log(111)
+}
+var offon = true; 
+    $(window).scroll(function () { //滚动浏览器就会执行
+          checkScroll()
+
+    }); 
+
+
+
+    function checkScroll(){
+        if (offon) {   //获取滚动高度
+              
+            var _top = $(window).scrollTop();  //滚动条高度   
+            var full_length = 0;
+            $('._list').each(function (i) {    //获取当前下标
+                   // console.log(i);
+                var _index = i;   
+                var _height = $('.mdui-container .mdui-row')[_index].offsetHeight; //获取上偏移值
+                full_length += _height;    //console.log(_index,full_length,_top)
+                if (full_length > _top) { //优先原则
+                        
+                    $('._list').eq(_index).addClass('mdui-list-item-active').siblings().removeClass(
+                        'mdui-list-item-active');    
+                    return false; //跳出遍历
+                       
+                };  
+            });  
+        }; 
+    }
+
+    $('._list').click(function () {  
+        offon = false;  
+        var _index = $(this).index('._list'); 
+        console.log(_index)
+        if ($('.mdui-container .mdui-row').eq(_index).length != 0) {
+            $('._list').removeClass('mdui-list-item-active');
+            $(this).addClass('mdui-list-item-active');  
+            var _height = $('.mdui-container .mdui-row').eq(_index).offset().top; //获取上偏移值
+              // console.log(_height)
+            $('body,html').animate({
+                scrollTop: _height - 64
+            }, 500, function () {   
+                offon = true;  
+            }); 
+        }
+
+    });
